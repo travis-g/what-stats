f599a29245461b7c95dfa5ff39657a4d7e572ae3
Travis Grammer <travisjgrammer@gmail.com>
2019-07-18 18:51:46 -0400

    add Gohu font submodule

---
+++ font_gohu/__init__.py
@@ -0,0 +1,19 @@
+import os
+import glob
+import string_utils
+
+font_directory = os.path.join(
+    os.path.abspath(os.path.dirname(__file__)), 'files')
+
+font_files = {
+# Gohufont11
+# Gohufont14
+# GohufontUni11
+# GohufontUni14
+}
+
+for font in list(glob.glob(os.path.join(font_directory, "*.ttf"))):
+    font_name = string_utils.snake_case_to_camel(
+        os.path.basename(font).replace(".ttf", ""), separator="-")
+    font_files[font_name] = font
+    globals()[font_name] = font
