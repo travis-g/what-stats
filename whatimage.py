665e926814cf6f4de061233f3b775d8be97393e5
Travis Grammer <travisjgrammer@gmail.com>
2019-07-18 20:00:50 -0400

    checkin

--- whatimage.py
+++ whatimage.py
@@ -1,29 +1,31 @@
 #!/usr/bin/env python
 
-import imgkit
-import argparse
 import requests
-import os
 import subprocess
 from font_gohu import GohufontUni14
 from StringIO import StringIO
+from PIL import Image, ImageFont, ImageDraw
+
+from stats import Stats
+
 try:
     from inky import InkyWHAT
-    _image_mode = "P"
+    _image_mode = "P" # palette-based
 except ImportError:
+    print("falling back to test library...")
     from testing import InkyWHAT
-    _image_mode = "RGB"
-from PIL import Image, ImageFont, ImageDraw
+    _image_mode = "RGB" # full rgb
 
 inky_display = InkyWHAT("red")
 inky_display.set_border(inky_display.WHITE)
 
 font = ImageFont.truetype(GohufontUni14, 14)
 
-fulltext = subprocess.check_output(['bar-usage'])
+fulltext = subprocess.check_output(['uptime'])
 fulltext = fulltext.decode('utf-8')
 
-img = Image.new(_image_mode, (inky_display.WIDTH, inky_display.HEIGHT), inky_display.WHITE)
+img = Image.new(_image_mode, (inky_display.WIDTH,
+                              inky_display.HEIGHT), inky_display.WHITE)
 draw = ImageDraw.Draw(img)
 
 draw.multiline_text((0, 0), fulltext, fill=inky_display.BLACK,
