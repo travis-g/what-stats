5d3d03270a5ec2c4d058a6ae4a3042c1ad167737
Travis Grammer <travisjgrammer@gmail.com>
2019-07-18 20:21:22 -0400

    autopep8 format

--- testing/inky.py
+++ testing/inky.py
@@ -6,8 +6,10 @@ BLACK = '#000'
 RED = '#f00'
 YELLOW = '#ff0'
 
+
 class InkyWHAT:
     """Fallback eInk Display Driver for the InkyWHAT"""
+
     def __init__(self, colour='red'):
         self.WHITE = WHITE
         self.BLACK = BLACK
@@ -16,13 +18,17 @@ class InkyWHAT:
         self.WIDTH = 400
         self.HEIGHT = 300
         self.image = Image.new("P", (self.WIDTH, self.HEIGHT))
+
     def show(self, busy_wait=True):
         self.image.show()
+
     def set_image(self, image):
         pal_img = Image.new("P", (1, 1))
-        pal_img.putpalette((255, 255, 255, 0, 0, 0, 255, 0, 0) + (0, 0, 0) * 252)
+        pal_img.putpalette(
+            (255, 255, 255, 0, 0, 0, 255, 0, 0) + (0, 0, 0) * 252)
 
         self.image = image.convert("RGB").quantize(palette=pal_img)
+
     def set_border(self, colour):
         # TODO
         return
